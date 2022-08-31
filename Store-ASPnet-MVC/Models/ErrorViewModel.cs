25dc102355a5375a15eb2aa966db1ddb2f2855d5
Apowmet <4ai6ezcaxapa@gmail.com>
2022-08-31 14:14:54 +0300

    Добавьте файлы проекта.

---
+++ Store-ASPnet-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Store_ASPnet_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
